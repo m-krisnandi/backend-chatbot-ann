bfe34243d33a56986cd8f9daaf7aec45cd1fd2fd
Muhamad Krisnandi <mkrisnandi08@gmail.com>
2023-06-19 09:16:55 +0700

    update device

--- chat.py
+++ chat.py
@@ -8,7 +8,7 @@ from model import NeuralNet
 from nltk_utils import bag_of_words, tokenize
 load_dotenv()
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cpu')
 
 
 # with open('intents.json', 'r') as json_data:
