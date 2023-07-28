607408076489b8002d5285899a08bf92be2f9d62
Muhamad Krisnandi <mkrisnandi08@gmail.com>
2023-07-29 06:02:08 +0700

    add print image_url, coordinates and add comment

--- train_with_graf.py
+++ train_with_graf.py
@@ -17,8 +17,10 @@ with open('intents.json', 'r') as f:
 all_words = []
 tags = []
 xy = []
+image_urls = [] # Menambahkan list untuk menyimpan image_url
+coordinates = [] # Menambahkan list untuk menyimpan coordinates
 
-# Melakukan pengulangan pada setiap kalimat di dalam intents patterns
+# Mengumpulkan data dari intents untuk pelatihan
 for intent in intents['intents']:
     tag = intent['tag']
     # Menambahkan ke dalam list tag
@@ -30,17 +32,28 @@ for intent in intents['intents']:
         all_words.extend(w)
         # Menambahkan ke dalam pasangan xy
         xy.append((w, tag))
+    # Memeriksa apakah terdapat key 'image_url' dalam intent dan menyimpannya dalam list image_urls
+    if 'image_url' in intent:
+        image_urls.append(intent['image_url'])
+    # Memeriksa apakah terdapat key 'coordinates' dalam intent dan menyimpannya dalam list coordinates
+    if 'coordinates' in intent:
+        coordinates.append(intent['coordinates'])
 
-# Melakukan stemming dan menghapus tanda baca
+# Preprocessing teks: stemming dan penghapusan tanda baca pada kata-kata
 ignore_words = ['?', '.', '!', ',', '"', "'", '`', '(', ')', '-', '[',
                 ']', '{', '}', ';', ':', '@', '#', '$', '%', '^', '&',
                 '*', '_', '+', '=', '\\', '|', '/', '~', '<', '>', '€']
 all_words = [stem(w) for w in all_words if w not in ignore_words]
 
-# Menghapus duplikasi dan melakukan pengurutan
+# Menghapus duplikasi dan melakukan pengurutan pada kata-kata dan tag
 all_words = sorted(set(all_words))
 tags = sorted(set(tags))
 
+# Menampilkan jumlah image_url dan coordinates yang telah ditemukan
+print(len(image_urls), "image_url")
+print(len(coordinates), "coordinates")
+
+# Menampilkan jumlah patterns, tags, dan unique stemmed words
 print(len(xy), "patterns")
 print(len(tags), "tags:", tags)
 print(len(all_words), "unique stemmed words:", all_words)
@@ -49,20 +62,22 @@ print(len(all_words), "unique stemmed words:", all_words)
 X_train = []
 y_train = []
 for (pattern_sentence, tag) in xy:
-    # X: bag of words untuk setiap pattern_sentence
+    # X: Konversi pattern_sentence menjadi bentuk bag of words
     bag = bag_of_words(pattern_sentence, all_words)
     X_train.append(bag)
-    # y: PyTorch CrossEntropyLoss hanya memerlukan label kelas
+    # y: PyTorch CrossEntropyLoss hanya memerlukan label kelas yang sesuai dengan index tag dalam tags
     label = tags.index(tag)
     y_train.append(label)
 
+# Konversi list menjadi numpy array untuk keperluan PyTorch
 X_train = np.array(X_train)
 y_train = np.array(y_train)
 
+# Menampilkan data pelatihan
 print("X_train: ", X_train[:, 0])
 print("y_train: ", y_train)
 
-# Hyperparameter
+# Definisikan hyperparameter untuk pelatihan model
 num_epochs = 1000
 batch_size = 8
 learning_rate = 0.001
@@ -108,39 +123,57 @@ model.eval()
 with torch.no_grad():
     correct = 0
     total = 0
-    epoch_loss = 0
 
 # Variabel untuk pelacakan
 loss_list = []
 accuracy_list = []
 
-# Loop pelatihan
+# Loop pelatihan model
 for epoch in range(num_epochs):
+    epoch_loss = 0 # Variabel untuk menyimpan total loss per epoch
+
+    # Memuat data pelatihan dalam batch
     for words, labels in train_loader:
+        # Memindahkan data batch ke device (GPU/CPU) yang digunakan
         words = words.to(device)
         labels = labels.to(device)
 
+        # Mengatur gradient ke 0 sebelum dilakukan backpropagation
         optimizer.zero_grad()
+
+        # Forward pass: menghitung output model saat ini
         outputs = model(words)
+
+        # Hitung loss dengan membandingkan output dan label
         loss = criterion(outputs, labels)
+
+        # Backward pass: hitung gradient loss
         loss.backward()
+
+        # Update model dengan optimizer (menggunakan algoritma Adam)
         optimizer.step()
 
+        # Akumulasi total loss per epoch
         epoch_loss += loss.item()
 
+        # Hitung akurasi batch ini
         _, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
 
+    # Rata-rata loss per epoch
     epoch_loss /= len(train_loader)
+
+    # Hitung akurasi per epoch
     accuracy = correct / total * 100
 
+    # Simpan loss dan akurasi tiap epoch
     loss_list.append(epoch_loss)
     accuracy_list.append(accuracy)
 
+    # Print loss dan akurasi per 100 epoch
     if (epoch + 1) % 100 == 0:
-        print(
-            f'Epoch [{epoch + 1}/{num_epochs}], Loss: {epoch_loss:.4f}, Accuracy: {accuracy:.2f}%')
+        print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {epoch_loss:.4f}, Accuracy: {accuracy:.2f}%')
 
 # Plot grafik untuk perkembangan pelatihan
 plt.plot(loss_list, label='Training Loss')
