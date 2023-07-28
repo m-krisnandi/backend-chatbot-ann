9f190cbf7692296694cb6f045ba1f2363aceb809
Muhamad Krisnandi <mkrisnandi08@gmail.com>
2023-07-29 06:54:50 +0700

    add webhook for auto response

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 from dotenv import load_dotenv
-from threading import Thread
-from flask import Flask
+from flask import Flask, request
 from chat import get_response
 import telegram
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
@@ -13,58 +12,69 @@ load_dotenv()
 bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
 bot = telegram.Bot(token=bot_token)
 
+# Fungsi handler untuk command /start
 def start(update, context):
     chat_id = update.message.chat_id
     context.bot.send_message(
         chat_id=chat_id, text="Halo. Selamat datang di chatbot informasi objek wisata di Kabupaten Bandung. Silahkan ketikkan pertanyaan anda.")
 
+# Fungsi handler untuk webhook
+@app.route('/' + bot_token, methods=['POST'])
+def webhook():
+    update = telegram.Update.de_json(request.get_json(force=True), bot)
+    dispatcher.process_update(update)
+    return 'ok'
 
 def echo(update, context):
+    """
+        Fungsi ini merespons pesan yang diterima dari pengguna.
+
+        Parameter:
+        - update: Objek yang berisi data terkait pesan yang diterima oleh bot.
+        - context: Objek yang menyimpan konteks dari pesan yang diterima, dan digunakan untuk berkomunikasi dengan Telegram API.
+    """
     chat_id = update.message.chat_id
     text = update.message.text
 
-    # check if text is valid
+    # Memeriksa apakah teks yang diterima valid dengan menggunakan fungsi get_response
     chat_response = get_response(text)
 
-    # send response to Telegram
+    # Mengirimkan respons teks ke Telegram (default)
+    response_message = context.bot.send_message(chat_id=chat_id, text=f"{chat_response.response}",
+                                                parse_mode=telegram.ParseMode.MARKDOWN)
+
+    # Memeriksa apakah terdapat image_url
     if chat_response.image_url:
-        response_message = context.bot.send_message(
-            chat_id=chat_id, text=f"{chat_response.response}\n\n*Mengirim gambar...*", parse_mode=telegram.ParseMode.MARKDOWN)
-        
-        # Dapatkan URL publik gambar yang diunggah
-        image_public_url = chat_response.image_url
-        
-        context.bot.send_photo(chat_id=chat_id, photo=image_public_url)
-        response_message.edit_text(f"{chat_response.response}\n\n*Gambar terkirim!*", parse_mode=telegram.ParseMode.MARKDOWN)
+        # Jika ada URL gambar, kirimkan gambar ke pengguna
+        context.bot.send_photo(chat_id=chat_id, photo=chat_response.image_url)
+        # Ubah pesan untuk memberitahu bahwa gambar telah terkirim
+        response_message.edit_text(f"{chat_response.response}\n\n*Gambar terkirim!*",
+                                   parse_mode=telegram.ParseMode.MARKDOWN)
 
-         # Tampilkan peta jika koordinat tersedia
-        if chat_response.coordinates:
-            lat, lon = chat_response.coordinates.split(',')
-            context.bot.send_message(chat_id=chat_id, text=f"*Lokasi di Peta:*", parse_mode=telegram.ParseMode.MARKDOWN)
-            context.bot.send_location(chat_id=chat_id, latitude=float(lat), longitude=float(lon))
-    else:
-        context.bot.send_message(chat_id=chat_id, text=f"{chat_response.response}", parse_mode=telegram.ParseMode.MARKDOWN)
+    # Memeriksa apakah terdapat koordinat
+    if chat_response.coordinates:
+        # Jika ada koordinat, tampilkan lokasi di peta
+        lat, lon = chat_response.coordinates.split(',')
+        context.bot.send_message(chat_id=chat_id, text=f"*Lokasi di Peta:*", parse_mode=telegram.ParseMode.MARKDOWN)
+        context.bot.send_location(chat_id=chat_id, latitude=float(lat), longitude=float(lon))
 
 
-updater = Updater(token=bot_token, use_context=True)
+# Fungsi untuk mengecek bot hidup
+@app.route('/ping')
+def ping():
+    return "Bot is alive!"
 
-updater.dispatcher.add_handler(CommandHandler("start", start))
-updater.dispatcher.add_handler(MessageHandler(
-    Filters.text & ~Filters.command, echo))
+# Dispatcher to handle bot commands
+dispatcher = Updater.dispatcher
+dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(MessageHandler(Filters.text, echo))
 
-
-def start_polling():
-    updater.start_polling()
-
-
-thread = Thread(target=start_polling)
-thread.start()
-
-
-@app.route('/')
-def home():
-    return "Server is running..."
+# Start webhook
+updater = Updater(bot_token, use_context=True)
+updater.start_webhook(listen="0.0.0.0",
+                      port=int(os.environ.get('PORT', 5000)),
+                      url_path=bot_token,
+                      webhook_url='https://<your-app-url>/' + bot_token)
 
 if __name__ == '__main__':
-    updater.start_polling()
     app.run(debug=True)
