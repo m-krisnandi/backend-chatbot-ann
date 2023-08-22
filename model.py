1ade1ac56f3e5e2ba5bec03b1950601b97cff31e
Muhamad Krisnandi <mkrisnandi08@gmail.com>
2023-08-22 10:54:36 +0700

    delete comment

--- model.py
+++ model.py
@@ -14,5 +14,4 @@ class ANeuralNet(nn.Module):
         out = self.l2(out)
         out = self.relu(out)
         out = self.l3(out)
-        # no activation and no softmax at the end
         return out
