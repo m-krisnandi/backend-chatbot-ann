f79235157f7b9519d27d3f7b2ef4eef220b1ce5f
Muhamad Krisnandi <mkrisnandi08@gmail.com>
2023-08-22 10:55:34 +0700

    add testing for each function

--- nltk_utils.py
+++ nltk_utils.py
@@ -4,7 +4,7 @@ nltk.download('stopwords')
 import numpy as np
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 
-factory = StemmerFactory() 
+factory = StemmerFactory()
 stemmer = factory.create_stemmer()
 
 indonesian_stopwords = nltk.corpus.stopwords.words('indonesian')
@@ -21,9 +21,8 @@ def bag_of_words(tokenized_sentence, all_words):
     """
     tokenized_sentence = ["info", "wisata", "kawah", "putih", "kabupaten", "bandung"]
     all_words = ["info", "wisata", "kawah", "putih", "di", "kabupaten", "bandung", "yang"]
-    bag = [0, 0, 1, 1, 1, 1, 0]
+    bag = [1. 1. 1. 1. 0. 1. 1. 0.]
     """
-
     tokenized_sentence = [stem(w) for w in tokenized_sentence]
     bag = np.zeros(len(all_words), dtype=np.float32)
     for idx, w in enumerate(all_words):
@@ -36,16 +35,30 @@ def bag_of_words(tokenized_sentence, all_words):
 # sentence = "Beritahu Saya dan Infokan Wisata Kawah Putih di Kabupaten Bandung!"
 # words = tokenize(sentence)
 # print(words)
+# Output: ['Beritahu', 'Saya', 'Infokan', 'Wisata', 'Kawah', 'Putih', 'Kabupaten', 'Bandung', '!']
 
-# Testing Stem
+# Testing Stopwords
+# tokenize_word = ["Beritahu", "Saya", "dan", "Infokan", "Wisata", "Kawah", "Putih", "di", "Kabupaten", "Bandung", "!"]
+# stopwords = [w for w in tokenize_word if w not in indonesian_stopwords]
+# print(stopwords)
+# Output: ['Beritahu', 'Infokan', 'Wisata', 'Kawah', 'Putih', 'Kabupaten', 'Bandung', '!']
+
+# Testing Case Folding
 # word = ['Beritahu', 'Saya', 'Infokan', 'Wisata', 'Kawah', 'Putih', 'Kabupaten', 'Bandung', '!']
-# stem = [stem(w) for w in word]
-# print(stem)
+# case_folding = [w.lower() for w in word]
+# print(case_folding)
+# Output: ['beritahu', 'saya', 'infokan', 'wisata', 'kawah', 'putih', 'kabupaten', 'bandung', '!']
+
+# Testing Stemming
+# words = ['beritahu', 'saya', 'infokan', 'wisata', 'kawah', 'putih', 'kabupaten', 'bandung', '!']
+# stemming = [stemmer.stem(w) for w in words]
+# print(stemming)
 
 # Testing Bag of Words
 # tokenized_sentence = ["info", "wisata", "kawah", "putih", "kabupaten", "bandung"]
 # all_words = ["info", "wisata", "kawah", "putih", "di", "kabupaten", "bandung", "yang"]
 # bag = bag_of_words(tokenized_sentence, all_words)
 # print(bag)
+# Output: [1. 1. 1. 1. 0. 1. 1. 0.]
 
 
