f724fd0e2af8ca6a2025e26a1e8ce4b6da0b2593
Muhamad Krisnandi <mkrisnandi08@gmail.com>
2023-08-22 10:56:01 +0700

    add testing scenario 2 and 3

--- testing.py
+++ testing.py
@@ -5,10 +5,13 @@ import numpy as np
 from nltk_utils import bag_of_words, tokenize, stem
 from model import ANeuralNet
 
-ignore_words = ['?', '.', '!', ',', '"', '’', '‘', '“', '”', '(', ')', '[', ']', '{', '}']
-
 # Load the trained model
+#testing 1
 data = torch.load("data.pth")
+#testing 2
+# data = torch.load("data2.pth")
+#testing 3
+# data = torch.load("data3.pth")
 input_size = data["input_size"]
 hidden_size = data["hidden_size"]
 output_size = data["output_size"]
@@ -27,12 +30,11 @@ with open('testing_data.json', 'r') as f:
 # Preprocess the testing data
 X_test = []
 y_test = []
-accuracies = []  # List to store accuracies for each sentence
 for test in testing_data:
     pattern_sentence = test['sentence']
     intent_tag = test['tag']
     tokenized_sentence = tokenize(pattern_sentence)
-    tokenized_sentence = [stem(w) for w in tokenized_sentence if w not in ignore_words]
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
     X_test.append(bag_of_words(tokenized_sentence, all_words))
     y_test.append(tags.index(intent_tag))
 
@@ -44,6 +46,7 @@ y_test = torch.from_numpy(y_test)
 
 # Evaluate the model on the testing data
 model.eval()
+incorrect_sentences = []  # List to store incorrect sentences
 with torch.no_grad():
     correct = 0
     total = 0
@@ -57,12 +60,19 @@ with torch.no_grad():
         correct += (predicted == labels).sum().item()
 
         accuracy = (predicted == labels).sum().item() / labels.size(0) * 100
-        accuracies.append(accuracy)
-        
+        if accuracy < 100:
+            incorrect_sentences.append(i + 1)  # Store the sentence number
+
+        tag = testing_data[i]['tag']
         sentence = testing_data[i]['sentence']
+        print(f"Tag: {i+1}: {tag}")
         print(f"Sentence {i+1}: {sentence}")
         print(f"Accuracy = {accuracy:.2f}%")
         print("--------------------")
 
 overall_accuracy = correct / total * 100
 print(f"\nOverall Accuracy: {overall_accuracy:.2f}%")
+
+if len(incorrect_sentences) > 0:
+    print("\nSentences with accuracy less than 100%:")
+    print(incorrect_sentences)
